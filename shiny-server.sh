809b8cff38c2e2ee03e8cca4efbc719670e2effb
Richard White <r.aubrey.white@gmail.com>
2016-05-18 10:20:55 +0200

    jkn
    kjn

--- shiny-server.sh
+++ shiny-server.sh
@@ -4,14 +4,4 @@
 mkdir -p /var/log/shiny-server
 chown shiny.shiny /var/log/shiny-server
 
-echo "server{
-    listen 3838 127.0.0.1;
-    
-    location / {
-    site_dir /srv/shiny-server;
-    log_dir /var/log/shiny-server;
-    directory_index on;
-    }
-}" > /etc/shiny-server/shiny-server.conf
-
 exec shiny-server >> /var/log/shiny-server.log 2>&1
